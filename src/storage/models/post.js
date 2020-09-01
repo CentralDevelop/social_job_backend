057d28d7aa214f28ea15089d3647ccbaa9df40dc
Omar Gonzalez <gonzalezomar645@gmail.com>
2020-08-31 19:16:06 -0500

    deleted schemaa that were extra

---
+++ src/storage/models/post.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const mySchema = new Schema({
+    title:String,
+    image:String ,// no sure
+    salary:Int32Array,
+    rating:Int32Array,
+    description:String,
+    company:String,
+    url:String,
+    skill:String
+    rate:Int32Array,
+    user: {
+        type:Schema.ObjectID,
+        ref:"User"
+    },
+    location:{
+        country:String,
+        city:String
+    },
+
+
+})
+
+const model = mongoose.model("Post", mySchema)
+
+module.exports = model
