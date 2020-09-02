66525725091de5cd31d985d754c7209fcfbd612f
Omar Gonzalez <gonzalezomar645@gmail.com>
2020-09-02 13:01:01 -0500

    fixing merge conflicts

--- src/storage/index.js
+++ src/storage/index.js
@@ -2,7 +2,6 @@ const db = require('mongoose')
 
 db.Promise = global.Promise
 
-<<<<<<< HEAD
 const connect = async (url) => {
   try {
     await db.connect(url, {
@@ -13,17 +12,5 @@ const connect = async (url) => {
   } catch (error) {
     console.log(error)
   }
-=======
-const connect = async () => {
-    try {
-        await db.connect(process.env.DB_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
-        console.log('[DB] Conecction success')
-    } catch (error) {
-        console.log(error)
-    }
->>>>>>> a75d62bdbee8f0f595f90a9aca6915ef8d51a49b
 }
 module.exports = connect
