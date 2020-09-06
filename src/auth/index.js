ed7cbcbeb5a0110631ddb5406e779813e4c65703
Omar Gonzalez <gonzalezomar645@gmail.com>
2020-09-05 22:06:25 -0500

    auth with update working

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,11 +1,12 @@
 require('dotenv').config()
 const jwt = require('jsonwebtoken')
 
+
 const createToken = (email, username) => {
     const token = jwt.sign({
         email,
         username
-    }, process.env.TOKEN_PHRS_KEY)
+    }, process.env.TOKEN_PHRS_KEY || "secret")
 
     return token
 }
